edea83bfbfe01f1ee2281bc7843df5ca329d1fe4
muhammadkashif20 <kashif28@gmail.com>
2024-12-30 10:19:22 +0500

    Working on Service Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
         </svg>
       </a>
-      <a className="ml-3 text-gray-500" href=''>
+      <a className="ml-3 text-gray-500" >
         <svg
           fill="currentColor"
           strokeLinecap="round"
